0649b71aece3932aadba224a05eb5511f8625551
Mark <mark.aaron.fisher@gmail.com>
2017-03-31 09:33:22 -0700

    add specs and user stories to README.md, create and save test and development databases, the latter pre-populated with some data

--- README.md
+++ README.md
@@ -1,20 +1,42 @@
-# _hair-salon_
+# Hair Salon App
 
-#### _hair-salon, 03-31-2017_
+#### Hair Salon App, 03-31-2017
 
-#### By _**Mark
-Mark**_
+#### By Mark Fisher
+
 
 ## Description
-_Example text for the description of the project_
+This application would help a hair salon manage it's clients and stylists.
+
+#### User stories
+- [ ] As a salon employee, I need to be able to see a list of all our stylists.
+- [ ] As an employee, I need to be able to select a stylist, see their details, and see a list of all clients that belong to that stylist.
+- [ ] As an employee, I need to add new stylists to our system when they are hired.
+- [ ] As an employee, I need to be able to add new clients to a specific stylist.
+- [ ] As an employee, I need to be able to update a stylist's details.
+- [ ] As an employee, I need to be able to update a client's details.
+- [ ] As an employee, I need to be able to delete a stylist if they're no longer employed here. (What happens to the clients?)
+- [ ] As an employee, I need to be able to delete a client if they no longer visit our salon.
+
 
 
 ## Specifications
 
 | Behavior                   | Input Example     | Output Example    |
 | -------------------------- | -----------------:| -----------------:|
+|User should be able to add a stylist|Add Naomi Smalls|Naomi Smalls added as a stylist|
+|User should be able to add a client to a stylist|Add Jiggly Caliente to Naomi Small's client list|Jiggly Caliente added to Naomi Small's client list|
+|User should be able to add a second client to a stylist's client list|Add Serena Cha Cha to Naomi Small's client list|Serena Cha Cha added to Naomi Small's client list|
+|User should be able to edit details about a client|Change Serena Cha Cha's address to Panama|Serena Cha Cha's address updated to reflect address change|
+|User should be able to remove a client from the system|Remove Serena Cha Cha completely|Serena Cha Cha removed|
+|User should be able to edit details about a stylist|Change Naomi Small's hours of operation to 9-5 on MWF|Naomi Small's hours of operation changed to 9-5 MWF|
+|User should be able to remove a stylist from the app|Remove Naomi Smalls|Naomi Smalls removed completely|
 
+### Bonus Specifications
 
+| Behavior                   | Input Example     | Output Example    |
+| -------------------------- | -----------------:| -----------------:|
+|If a stylist is removed, clients should be randomly assigned to remaining stylists|Naomi Smalls removed|Jiggly Caliente assigned to Raja and Serena Cha Cha assigned to Kim Chi|
 
 ## Setup/Installation Requirements
 
@@ -25,7 +47,6 @@ _Example text for the description of the project_
 
 ### License
 
-Copyright (c) 2017 **_Mark
-Mark_**
+Copyright (c) 2017 Mark Fisher
 
 This software is licensed under the MIT license.
---
+++ hair_salon.sql
@@ -0,0 +1,181 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.2
+-- Dumped by pg_dump version 9.6.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: clients; Type: TABLE; Schema: public; Owner: mf
+--
+
+CREATE TABLE clients (
+    id integer NOT NULL,
+    stylistid integer,
+    name character varying,
+    phone character varying,
+    address character varying,
+    email character varying,
+    special_details character varying,
+    preferred_stylist character varying,
+    emergency_name character varying,
+    emergency_phone character varying,
+    ok_to_text boolean
+);
+
+
+ALTER TABLE clients OWNER TO mf;
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE; Schema: public; Owner: mf
+--
+
+CREATE SEQUENCE clients_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE clients_id_seq OWNER TO mf;
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: mf
+--
+
+ALTER SEQUENCE clients_id_seq OWNED BY clients.id;
+
+
+--
+-- Name: stylists; Type: TABLE; Schema: public; Owner: mf
+--
+
+CREATE TABLE stylists (
+    id integer NOT NULL,
+    name character varying,
+    hours character varying,
+    specialties character varying
+);
+
+
+ALTER TABLE stylists OWNER TO mf;
+
+--
+-- Name: stylists_id_seq; Type: SEQUENCE; Schema: public; Owner: mf
+--
+
+CREATE SEQUENCE stylists_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE stylists_id_seq OWNER TO mf;
+
+--
+-- Name: stylists_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: mf
+--
+
+ALTER SEQUENCE stylists_id_seq OWNED BY stylists.id;
+
+
+--
+-- Name: clients id; Type: DEFAULT; Schema: public; Owner: mf
+--
+
+ALTER TABLE ONLY clients ALTER COLUMN id SET DEFAULT nextval('clients_id_seq'::regclass);
+
+
+--
+-- Name: stylists id; Type: DEFAULT; Schema: public; Owner: mf
+--
+
+ALTER TABLE ONLY stylists ALTER COLUMN id SET DEFAULT nextval('stylists_id_seq'::regclass);
+
+
+--
+-- Data for Name: clients; Type: TABLE DATA; Schema: public; Owner: mf
+--
+
+COPY clients (id, stylistid, name, phone, address, email, special_details, preferred_stylist, emergency_name, emergency_phone, ok_to_text) FROM stdin;
+1	1	Magnolia Crawford	5556667777	123 Forgotten Lane Portland, OR 97203	neverbooked@gmail.com	frequent no-show	Naomi Smalls	Ron Swanson	5556667778	t
+2	1	Jiggly Caliente	5556668888	123 Baked Potato Lane Portland, OR 97203	bustedgrill@gmail.com	chatty client	Naomi Smalls	Ronald McDonald	5556668889	f
+\.
+
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: mf
+--
+
+SELECT pg_catalog.setval('clients_id_seq', 2, true);
+
+
+--
+-- Data for Name: stylists; Type: TABLE DATA; Schema: public; Owner: mf
+--
+
+COPY stylists (id, name, hours, specialties) FROM stdin;
+1	Naomi Smalls	09:00-17:00MWF	hair, color, extensions
+2	Kim Chi	09:00-17:00TR	hair, color
+\.
+
+
+--
+-- Name: stylists_id_seq; Type: SEQUENCE SET; Schema: public; Owner: mf
+--
+
+SELECT pg_catalog.setval('stylists_id_seq', 2, true);
+
+
+--
+-- Name: clients clients_pkey; Type: CONSTRAINT; Schema: public; Owner: mf
+--
+
+ALTER TABLE ONLY clients
+    ADD CONSTRAINT clients_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: stylists stylists_pkey; Type: CONSTRAINT; Schema: public; Owner: mf
+--
+
+ALTER TABLE ONLY stylists
+    ADD CONSTRAINT stylists_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
