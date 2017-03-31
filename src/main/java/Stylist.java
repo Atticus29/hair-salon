25594122822b008b8e4032c3ae0a70d17e056983
Mark <mark.aaron.fisher@gmail.com>
2017-03-31 10:12:08 -0700

    add getters and constructor for stylist class and tests thereof

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,9 +1,33 @@
 import org.sql2o.*;
 
 public class Stylist {
+  private int id;
+  private String name;
+  private String hours;
+  private String specialties;
 
-  public Stylist() {
+  public Stylist(String name, String hours, String specialties) {
+    this.name = name;
+    this.hours = hours;
+    this.specialties = specialties;
+  }
 
+  // Add validate hours method
+
+  public String getName(){
+    return this.name;
+  }
+
+  public String getHours(){
+    return this.hours;
+  }
+
+  public String getSpecialties(){
+    return this.specialties;
+  }
+
+  public int getId(){
+    return this.id;
   }
 
 }
