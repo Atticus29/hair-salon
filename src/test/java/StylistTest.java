28d40c4d474d34383394d582639d79c15a11da65
Mark <mark.aaron.fisher@gmail.com>
2017-03-31 10:38:57 -0700

    add equals, save, all, and find methods with attendant passing tests

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,6 +1,7 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class StylistTest {
   private Stylist testStylist;
@@ -11,6 +12,7 @@ public class StylistTest {
   @Before
   public void setUp() {
     testStylist = new Stylist ("Raven", "08:00-16:00MSN", "hair, corn rows");
+    testStylist.save();
   }
 
   @Test
@@ -23,6 +25,36 @@ public class StylistTest {
     assertEquals("Raven", testStylist.getName());
     assertEquals("08:00-16:00MSN", testStylist.getHours());
     assertEquals("hair, corn rows", testStylist.getSpecialties());
+  }
+
+  @Test
+  public void all_returnsListOfStylists_true(){
+    Stylist testStylist2 = new Stylist ("Raven", "08:00-16:00MSN", "hair, corn rows");
+    testStylist2.save();
+    List<Stylist> results = Stylist.all();
+    assertEquals(2, results.size());
+    assertEquals(false, results.get(0).equals(results.get(1)));
+  }
+
+  @Test
+  public void save_addsEntryToDatabase_true(){
+    Stylist retrievedStylist = Stylist.find(testStylist.getId());
+    assertEquals(testStylist, retrievedStylist);
+  }
+
+  @Test
+  public void find_returnsSecondStylist_true(){
+    Stylist testStylist2 = new Stylist ("Raven", "08:00-16:00MSN", "hair, corn rows");
+    testStylist2.save();
+    int Stylist2Id = testStylist2.getId();
+    assertEquals(testStylist2, Stylist.find(Stylist2Id));
+  }
+
+  @Test
+  public void equals_correctlyAssessesTheEqualityOfStylistObjects_true(){
+    Stylist testStylist2 = new Stylist ("Raven", "08:00-16:00MSN", "hair, corn rows");
+    testStylist2.setId(testStylist.getId());
+    assertEquals(testStylist, testStylist2);
   }
 
 }
